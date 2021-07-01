08ac5420b2ca591fd02aa0356f13afa3121ddd7e
EC2 Default User <ec2-user@ip-172-31-27-10.ec2.internal>
2021-07-01 22:12:22 +0000

    With images

--- src/pets.py
+++ src/pets.py
@@ -27,3 +27,27 @@ def getAllPets(event, context):
          },
         'body': json.dumps(items)
     }
+    
+def getPet(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    pet_id = array_path[-1]
+    print(pet_id)
+    
+    response = table.query(
+        KeyConditionExpression=Key('PK').eq(pet_id) & Key('SK').begins_with('image')
+    )
+    
+    items = response['Items']
+    print(items)
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
+            "Access-Control-Allow-Credentials" : True # Required for cookies, authorization headers with HTTPS 
+         },
+        'body': json.dumps(items)
+    }
